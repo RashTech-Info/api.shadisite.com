6ac15b10f8f91bba316240e133d1c79c2c9adbb6
RashTechInfo <rashtechinfo@gmail.com>
2025-07-11 18:34:51 +0530

    main

--- src/routers/user/user_login.js
+++ src/routers/user/user_login.js
@@ -7,23 +7,13 @@ const passport = require("passport");
 const jwt = require("jsonwebtoken");
 
 router.post("/user_login", userLogin);
-// ✅ Step 1: Store selected role in cookie and redirect to Google
-// router.get("/auth/google/set-role", (req, res) => {
-//   res.cookie("selectedRole", role, {
-//     maxAge: 5 * 60 * 1000, // Valid for 5 minutes
-//     httpOnly: true,
-//   });
-
-//   res.redirect("/auth/google");
-// });
-
-// ✅ Step 2: Trigger Google OAuth
+// ✅ Step 1: Trigger Google OAuth
 router.get(
   "/auth/google",
   passport.authenticate("google", { scope: ["profile", "email"] })
 );
 
-// // ✅ Step 3: Handle Google callback
+// // ✅ Step 2: Handle Google callback
 router.get(
   "/auth/google/callback",
   passport.authenticate("google", { session: false }),
@@ -41,7 +31,7 @@ router.get(
         maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
       });
 
-      res.redirect(`http://localhost:5173/${redirectPath}`);
+      res.redirect(`https://shadisite.com/${redirectPath}`);
     } catch (error) {
       console.error("OAuth Callback Error:", error);
       res.status(500).json({ message: "Internal server error" });
